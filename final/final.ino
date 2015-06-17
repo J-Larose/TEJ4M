8e70f23beb8b761bbf147bca1561e6e1d4fb57f8
thevictor889 <thisisntvictor@hotmail.ca>
2015-06-17 14:26:44 -0400

    final

--- final/final.ino
+++ final/final.ino
@@ -6,16 +6,22 @@ int Y = 0;
 int X = 1;
 int value1 = 0;
 int value2 = 0;
+const int trigPin = 2;
+const int echoPin = 4;
+long duration, inches;
+boolean high = true;
+
  
 void setup() {
   xAxis.attach(9);
-  xAxis.write(81);  
+  xAxis.write(90);  
   Serial.begin(9600);
 }
 
 void loop() {
-  //joyRead();
-  //xAxisMotor();
+  joyRead();
+  checkClose();
+  xAxisMotor();
   delay(50);
 }
 
@@ -31,5 +37,53 @@ void joyRead() {
 }
 
 void xAxisMotor(){
-  xAxis.write(value2);  
+  if (inches<4){
+    xAxis.write(90);
+  }else{
+    if (value2 >400){
+      xAxis.write(180); 
+    }else if (value2 <300){
+      xAxis.write(0);
+    }else {
+      xAxis.write(90);
+    }  
+  }  
+}
+
+void checkClose(){
+ /*
+  pinMode(trigPin, OUTPUT);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // convert the time into a distance
+  inches = microsecondsToInches(duration);
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  */
+  Serial.println(analogRead(5));
+  
+  if (analogRead(5)>1000){
+    inches=1;
+    Serial.println ("HIHG");
+    while (analogRead(5)>1000){
+      xAxis.write(180);
+    }
+  }else{
+    inches=6;
+    Serial.println ("LOW");
+  }
+
+}
+ 
+long microsecondsToInches(long microseconds)
+{
+  return microseconds / 74 / 2;
 }
